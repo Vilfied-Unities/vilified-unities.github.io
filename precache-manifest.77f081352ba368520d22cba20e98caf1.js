367bca9c2c593f35c36cd52e38fe87759a541bce
sambodjango <gringolalia@gmail.com>
2020-12-27 17:16:52 -0300

    deploy

---
+++ precache-manifest.77f081352ba368520d22cba20e98caf1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dc507acecb05e2457ae",
+    "url": "/css/app.942991b3.css"
+  },
+  {
+    "revision": "7a16fd68dfe9946ac1bd",
+    "url": "/css/chunk-vendors.bda033dc.css"
+  },
+  {
+    "revision": "f9e888c0a13c5e776c422f30302d81c7",
+    "url": "/img/nyc-more-chrysler.f9e888c0.jpg"
+  },
+  {
+    "revision": "a73499292cb77f0cdecc9ecbd8168187",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ff207381d7e70fc6c101",
+    "url": "/js/about.7347313d.js"
+  },
+  {
+    "revision": "2dc507acecb05e2457ae",
+    "url": "/js/app.b16b87b1.js"
+  },
+  {
+    "revision": "7a16fd68dfe9946ac1bd",
+    "url": "/js/chunk-vendors.486ab6d9.js"
+  },
+  {
+    "revision": "51db8109139cb9a20fe851f3980d17de",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
